efb781ea239a27e21ae5071ea967102cb8c0e857
vaheaslanyan <vahe.sde@gmail.com>
2022-11-07 16:27:11 -0600

    Add MergeSort

--- src/main/java/com/urartusoftware/Launcher.java
+++ src/main/java/com/urartusoftware/Launcher.java
@@ -1,26 +1,34 @@
 package com.urartusoftware;
 
+import com.urartusoftware.algorithms.MergeSort;
 import com.urartusoftware.structures.*;
+
+import java.util.Arrays;
 
 public class Launcher {
 
+    static MergeSort ms = new MergeSort();
+
     public static void main(String[] args) {
-        int[] intArr = { 20, 35, -15, 7, 55, 1, -22};
-
-        Employee joseph = new Employee("Hoseph", "Kevorkian", 1);
-        Employee savo = new Employee("Saverkis", "Hambardumian", 314);
-        Employee lamish = new Employee("Lamo", "Samo", 412);
-        Employee gago = new Employee("Gago", "Drago", 2);
-        Employee prosht = new Employee("Proshian", "Halkanian", 452);
-
-        ChainedHashtableStructure ht = new ChainedHashtableStructure();
-        ht.put("Hoseph", joseph);
-        ht.put("Saverkis", savo);
-        ht.put("Lamo", lamish);
-        ht.put("Gago", gago);
-
+//        int[] intArr = { 20, 35, -15, 7, 55, 1, -22};
+//
+//        Employee joseph = new Employee("Hoseph", "Kevorkian", 1);
+//        Employee savo = new Employee("Saverkis", "Hambardumian", 314);
+//        Employee lamish = new Employee("Lamo", "Samo", 412);
+//        Employee gago = new Employee("Gago", "Drago", 2);
+//        Employee prosht = new Employee("Proshian", "Halkanian", 452);
+//
+//        ChainedHashtableStructure ht = new ChainedHashtableStructure();
+//        ht.put("Hoseph", joseph);
+//        ht.put("Saverkis", savo);
+//        ht.put("Lamo", lamish);
+//        ht.put("Gago", gago);
+//
+////        ht.printHashtable();
+//
 //        ht.printHashtable();
 
-        ht.printHashtable();
+        int[] unsortedArr = {7, 12, 5, 2, 6, 1, 63, 0};
+        System.out.println(Arrays.toString(ms.mergeSort(unsortedArr)));
     }
 }
--- src/main/java/com/urartusoftware/algorithms/MergeSort.java
+++ src/main/java/com/urartusoftware/algorithms/MergeSort.java
@@ -3,4 +3,61 @@ package com.urartusoftware.algorithms;
 // Splits and merges arrays
 // Usually is achieved using recursion
 public class MergeSort {
+
+    public int[] mergeSort(int[] inputArray) {
+        int inputLength = inputArray.length;
+
+        if (inputLength < 2) {
+            return null;
+        }
+
+        int midIndex = inputLength / 2;
+        int[] leftHalf = new int[midIndex];
+        int[] rightHalf = new int[inputLength - midIndex];
+
+        for (int i = 0; i < midIndex; i++) {
+            leftHalf[i] = inputArray[i];
+        }
+
+        for (int i = midIndex; i < inputLength; i++) {
+            rightHalf[i - midIndex] = inputArray[i];
+        }
+
+        mergeSort(leftHalf);
+        mergeSort(rightHalf);
+
+        merge(inputArray, leftHalf, rightHalf);
+
+        return inputArray;
+    }
+
+    private static void merge(int[] inputArray, int[] leftHalf, int[] rightHalf) {
+        int leftSize = leftHalf.length;
+        int rightSize = rightHalf.length;
+
+        int leftIndex = 0, rightIndex = 0, mainIndex = 0;
+
+        while (leftIndex < leftSize && rightIndex < rightSize) {
+            if (leftHalf[leftIndex] <= rightHalf[rightIndex]) {
+                inputArray[mainIndex] = leftHalf[leftIndex];
+                leftIndex++;
+            } else {
+                inputArray[mainIndex] = rightHalf[rightIndex];
+                rightIndex++;
+            }
+            mainIndex++;
+        }
+
+        while (leftIndex < leftSize) {
+            inputArray[mainIndex] = leftHalf[leftIndex];
+            leftIndex++;
+            mainIndex++;
+        }
+
+        while (rightIndex < rightSize) {
+            inputArray[mainIndex] = rightHalf[rightIndex];
+            rightIndex++;
+            mainIndex++;
+        }
+    }
 }
